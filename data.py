17cb11de45384e799a3f97a6f8385b2216fa67f9
Saveliyburk <63881498+Saveliyburk@users.noreply.github.com>
2020-11-05 16:17:40 +0300

    GenBot
    CreateBot

---
+++ data.py
@@ -0,0 +1,71 @@
+'''Helper by Saveliy Burkov is licensed under CC BY-SA 4.0.'''
+# Формирование данных в нужном формате о дежурных
+
+
+HumDej1 = {
+    'name': 'NAME First/Second1',
+    'num': 'Phone1',
+    'skype': 'LoginMessenger1'
+}
+HumDej1D = str(HumDej1['name'] + '\n' + HumDej1['skype'] + '\n' + HumDej1['num'])
+HumDej2 = {
+    'name': 'NAME First/Second2',
+    'num': 'Phone2',
+    'skype': 'LoginMessenger2'
+}
+HumDej2D = str(HumDej2['name'] + '\n' + HumDej2['skype'] + '\n' + HumDej2['num'])
+HumDej3 = {
+    'name': 'NAME First/Second3',
+    'num': 'Phone3',
+    'skype': 'LoginMessenger3'
+}
+HumDej3D = str(HumDej3['name'] + '\n' + HumDej3['skype'] + '\n' + HumDej3['num'])
+HumDej4 = {
+    'name': 'NAME First/Second4',
+    'num': 'Phone4',
+    'skype': 'LoginMessenger4'
+}
+HumDej4D = str(HumDej4['name'] + '\n' + HumDej4['skype'] + '\n' + HumDej4['num'])
+HumDej5 = {
+    'name': 'NAME First/Second5',
+    'num': 'Phone5',
+    'skype': ' LoginMessenger5'
+}
+HumDej5D = str(HumDej5['name'] + '\n' + HumDej5['skype'] + '\n' + HumDej5['num'])
+HumDej6 = {
+    'name': 'NAME First/Second6',
+    'num': 'Phone6',
+    'skype': 'LoginMessenger6'
+}
+HumDej6D = str(HumDej6['name'] + '\n' + HumDej6['skype'] + '\n' + HumDej6['num'])
+HumDej7 = {
+    'name': 'NAME First/Second7',
+    'num': 'Phone7',
+    'skype': 'LoginMessenger7'
+}
+HumDej7D = str(HumDej7['name'] + '\n' + HumDej7['skype'] + '\n' + HumDej7['num'])
+HumDej8 = {
+    'name': 'NAME First/Second8',
+    'num': 'Phone8',
+    'skype': 'LoginMessenger8'
+}
+HumDej8D = str(HumDej8['name'] + '\n' + HumDej8['skype'] + '\n' + HumDej8['num'])
+HumDej9 = {
+    'name': 'NAME First/Second9',
+    'num': 'Phone9',
+    'skype': 'LoginMessenger9'
+}
+HumDej9D = str(HumDej9['name'] + '\n' + HumDej9['skype'] + '\n' + HumDej9['num'])
+HumDej10 = {
+    'name': 'NAME First/Second10',
+    'num': 'Phone10',
+    'skype': 'LoginMessenger10'
+}
+HumDej10D = str(HumDej10['name'] + '\n' + HumDej10['skype'] + '\n' + HumDej10['num'])
+NaND = str('DefaultData')
+
+# время
+t12 = str('12:00')
+t15 = str('15:45')
+t17 = str('17:00')
+t20 = str('20:00')
---
+++ secret.py
@@ -0,0 +1,2 @@
+'''Helper by Saveliy Burkov is licensed under CC BY-SA 4.0.'''
+TOKEN = '' #TG-Token
